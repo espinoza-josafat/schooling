35ac5e1a226c5c78a5419128a6ba303255c23dfb
espinoza-josafat <38735885+espinoza-josafat@users.noreply.github.com>
2018-08-15 12:34:47 -0500

    SignUp
    SignUp

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,114 @@
 import React from "react";
 
-export default class SignUpPage extends React.Component {
+import { Link, withRouter } from "react-router-dom";
+
+import * as routes from "../constants/routes";
+
+import auth from "../firebase";
+
+const SignUpPage = () => (
+  <div>
+    <h1>SignUp</h1>
+    <SignUpForm history={history} />
+  </div>
+);
+
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
+};
+
+const byPropKey = (proppertyName, value) => () => ({
+  [proppertyName]: value
+});
+
+class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { username, email, passwordOne } = this.state;
+    const { history } = this.props;
+
+    auth
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+        history.push(routes.HOME);
+      })
+      .catch(error => {
+        this.setState(byPropKey("error", error));
+      });
+
+    event.preventDefault();
+  };
+
   render() {
+    const { username, email, passwordOne, passwordTwo, error } = this.state;
+    const isValid =
+      passwordOne != passwordTwo ||
+      passwordOne === "" ||
+      passwordTwo === "" ||
+      email === "" ||
+      username === "";
     return (
-      <div>
-        <h1>SignUp Page</h1>
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          onChange={event =>
+            this.setState(byPropKey("username", event.target.value))
+          }
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          value={email}
+          onChange={event =>
+            this.setState(byPropKey("email", event.target.value))
+          }
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={passwordOne}
+          onChange={event =>
+            this.setState(byPropKey("passwordOne", event.target.value))
+          }
+          type="text"
+          placeholder="Password"
+        />
+        <input
+          value={passwordTwo}
+          onChange={event =>
+            this.setState(byPropKey("passwordTwo", event.target.value))
+          }
+          type="text"
+          placeholder="Confirm Password"
+        />
+
+        <button disabled={isValid} type="submit">
+          Sign Up
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
     );
   }
 }
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={routes.SIGN_UP}>Sign Up></Link>
+  </p>
+);
+
+//export default SignUpPage;
+
+export default withRouter(SignUpPage);
+
+export { SignUpForm, SignUpLink };
---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBu84S-EeyWVOAtgW95Zs_y6BEF2meQcSs",
+  authDomain: "schooling-mx.firebaseapp.com",
+  databaseURL: "https://schooling-mx.firebaseio.com",
+  projectId: "schooling-mx",
+  storageBucket: "schooling-mx.appspot.com",
+  messagingSenderId: "220464955862"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export { auth };
